d44e84dbb12a7a8773f51a84b2075f2564b7cc58
Tetyana Gogulya <cheburashka077@ukr.net>
2024-09-14 21:30:26 +0300

    Tetyana_logout

--- src/components/Modals/LogOut/LogOut.jsx
+++ src/components/Modals/LogOut/LogOut.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import css from './LogOut.module.css';
+import { useContext } from '../../../context/useContext.jsx';
+import { useDispatch } from 'react-redux';
+import { logOutUser } from '../../../redux/auth/operation.js';
+import { useTranslation } from 'react-i18next';
+import toast from 'react-hot-toast';
 
-const LogOut = () => {
-    return <div>LogOut</div>;
+const LogOutModalWind = () => {
+    const { closeModal } = useContext();
+    const dispatch = useDispatch();
+    const { t } = useTranslation();
+
+    return (
+        <div className={css.modalWindCont}>
+            <div className={css.titleText}>
+                <h2 className={css.title}>{t('modals.logOut.title')}</h2>
+                <p className={css.text}>{t('modals.logOut.text')}</p>
+            </div>
+            <div className={css.modalButtonCont}>
+                <button
+                    className={logoutButton}
+                    onClick={() => {
+                        dispatch(logOutUser());
+                        closeModal();
+                        toast.success(t('toast.logOut'));
+                    }}
+                >
+                    {t('modals.logOut.logOut')}
+                </button>
+                <button className={css.cancelButton} onClick={closeModal}>
+                    {t('modals.logOut.cancel')}
+                </button>
+            </div>
+        </div>
+    );
 };
 
-export default LogOut;
+export default LogOutModalWind;
