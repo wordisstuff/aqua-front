895084e23802d4635f51ab0029418ec5ae2a0d42
Dmytro Tkach <dreamworkshow@gmail.com>
2024-09-14 18:30:05 +0200

    redux user createAsyncThunk

---
+++ src/helpers/axsiosConfig.js
@@ -0,0 +1,6 @@
+// npm install axios
+import axios from 'axios';
+
+axios.defaults.baseURL = ''; // ссылка на render.com
+
+export default axios;
--- src/redux/users/operation.js
+++ src/redux/users/operation.js
@@ -1,3 +1,16 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-export const getUsers = createAsyncThunk('users/get', async () => {});
+import axios from '../../helpers/axsiosConfig';
+
+// npm install react-hot-toast
+import toast from 'react-hot-toast';
+
+export const getUsers = createAsyncThunk('users/get', async (reduxAPI) => {
+    try {
+        const response = await axios.get('/users/happy'); // смайлик с библиотеки react-hot-toast (happy)
+        return response.data;
+    } catch (error) {
+        toast.error(error.response.data.message);
+        return reduxAPI.rejectWithValue(error.message);
+    }
+});
