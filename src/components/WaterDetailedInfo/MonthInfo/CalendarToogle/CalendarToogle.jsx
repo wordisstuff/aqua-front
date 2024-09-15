37cb052e55e5beb64ff01a382fc0be45bfb1967b
12DaNa21 <145206474+12DaNa21@users.noreply.github.com>
2024-09-15 23:43:39 +0300

    created calendar and monthinfo component

---
+++ src/components/WaterDetailedInfo/MonthInfo/CalendarToogle/CalendarToogle.jsx
@@ -0,0 +1,18 @@
+import css from './CalendarToggle.module.css';
+import { icons as sprite } from '../../../../utils/icons/index';
+
+const CalendarToggle = ({ isActive, setIsActive }) => {
+    const toggleClass = () => {
+        setIsActive(!isActive);
+    };
+
+    return (
+        <button className={isActive ? css.active : ''} onClick={toggleClass}>
+            <svg className={css.svg}>
+                <use xlinkHref={`${sprite}#pie-chart-02-1`} />
+            </svg>
+        </button>
+    );
+};
+
+export default CalendarToggle;
