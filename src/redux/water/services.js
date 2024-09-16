00d06cb55204d6be12bd7f6ad7c069d3d560db61
Tetyana Gogulya <cheburashka077@ukr.net>
2024-09-17 00:34:58 +0300

    corr_3

--- src/redux/water/services.js
+++ src/redux/water/services.js
@@ -1,17 +1,17 @@
 import axios from '../../helpers/axsiosConfig.js';
 
 export const requestDeleteWater = async (waterId) => {
-    const { data } = await axios.delete(`/water/${waterId}`); //всі роути запитів не звіряла з беком, можливі відмінності
+    const { data } = await axios.delete(`/water/${waterId}`); 
     return data;
 };
 
 export const requestGetWaterDay = async (date) => {
-    const { data } = await axios.get(`water/daily/${date}`);//всі роути запитів не звіряла з беком, можливі відмінності
+    const { data } = await axios.get(`water/daily/${date}`);
     return data;
 };
 
 export const requestGetWaterMonth = async (date) => {
-    const { data } = await axios.get(`/water/monthly/${date.year}/${date / month}`);//всі роути запитів не звіряла з беком, можливі відмінності
+    const { data } = await axios.get(`/water/monthly/${date.year}/${date / month}`);
     return data;
 };
 
