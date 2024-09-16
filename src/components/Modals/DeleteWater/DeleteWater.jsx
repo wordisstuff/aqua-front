60c8ba14ddf9d2b56b35554491ee2a059af3c0b1
Tetyana Gogulya <cheburashka077@ukr.net>
2024-09-16 20:28:36 +0300

    corr_1

--- src/components/Modals/DeleteWater/DeleteWater.jsx
+++ src/components/Modals/DeleteWater/DeleteWater.jsx
@@ -1,4 +1,14 @@
-const DeleteWater = () => {
+import { useTranslation } from 'react-i18next';
+import { useContext } from '../../../context/useContext.jsx';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectDate } from '../../../redux/water/selectors.js';
+
+const DeleteWater = ({ onDelete }) => {
+    const { t } = useTranslation();
+    const { closeModal } = useContext();
+    const dispatch = useDispatch();
+    const selectedDate = useSelector(selectDate);
+
     return <div>DeleteWater</div>;
 };
 
