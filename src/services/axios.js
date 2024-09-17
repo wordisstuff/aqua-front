7a5a8efa4539343affa05726c58293895514bb8f
wordisstuff <wordisstuff@gmail.com>
2024-09-17 15:09:27 -0500

    Update v2.1

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 export const aquaApi = axios.create({
-    baseURL: 'http://localhost:8080',
-    // || 'https://aqua-back.onrender.com',
+    baseURL: 'http://localhost:8080' || 'https://aqua-back.onrender.com',
 });
 export const setAuthHeader = token => {
     aquaApi.defaults.headers.common.Authorization = `Bearer ${token}`;
