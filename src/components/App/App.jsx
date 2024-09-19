128911baceaefc12dd3bd7ec7de8d3d859408172
RadchenkoArtem2023 <143721793+RadchenkoArtem2023@users.noreply.github.com>
2024-09-19 23:12:11 +0300

    add_task

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,6 +9,7 @@ import RestrictRoute from '../RestrictedRoute.jsx';
 import { PrivateRoute } from '../PrivateRoute.jsx';
 import VerifyEmail from '../VerifyEmail/VerifyEmail.jsx';
 // import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage.jsx';
+const ForgotPage = lazy(() => import('../../pages/ForgotPage/ForgotPage.jsx'));
 const SignUpPage = lazy(() => import('../../pages/SignUpPage/SignUpPage.jsx'));
 const SignInPage = lazy(() => import('../../pages/SignInPage/SignInPage.jsx'));
 const HomePage = lazy(() => import('../../pages/HomePage/HomePage.jsx'));
@@ -27,6 +28,7 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<Layout />} />
                     <Route index element={<HomePage />} />
+                    <Route path="/forgotPassword" element={<ForgotPage />} />
                     <Route path="/signup" element={<SignUpPage />} />
                     <Route
                         path="/signin"
---
+++ src/pages/ForgotPage/ForgotPage.jsx
@@ -0,0 +1,32 @@
+import { Helmet } from 'react-helmet-async';
+import css from './ForgotPage.module.css';
+import { useTranslation } from 'react-i18next';
+import Additional from '../../components/Home/Additional.jsx';
+import useMedia from '../../helpers/useHooks/useMedia.js';
+import Container from '../../components/ShareComponents/Container/Container.jsx';
+import Languages from '../../components/ShareComponents/Languages/Languages.jsx';
+import ForgotPageForm from '../../components/ForgotPageForm/ForgotPageForm.jsx';
+
+const ForgotPage = () => {
+    const { t } = useTranslation();
+    const { desktop } = useMedia();
+
+    return (
+        <>
+            <Helmet>
+                <title>{t('page.forgotPassword')}</title>
+            </Helmet>
+            <Container>
+                <div className={css.containerPage}>
+                    <Languages />
+                    <div className={css.containerForm}>
+                        <ForgotPageForm />
+                        {desktop && <Additional />}
+                    </div>
+                </div>
+            </Container>
+        </>
+    );
+};
+
+export default ForgotPage;
