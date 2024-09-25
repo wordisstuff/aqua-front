ee1a1e8c8392f9473e60c16ab85dc6354c011f43
Olena457 <olenademianenko42@gmail.com>
2024-09-25 17:12:13 +0300

    corected style page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,29 +1,49 @@
-import { Helmet } from 'react-helmet-async';
-import css from './NotFoundPage.module.css';
-import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import Container from '../../components/ShareComponents/Container/Container';
+import Container from '../../components/ShareComponents/Container/Container.jsx';
+import { Helmet } from 'react-helmet-async';
+import { FaArrowDown } from 'react-icons/fa';
+import { motion } from 'framer-motion';
+import css from './NotFoundPage.module.css';
+import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
     const { t } = useTranslation();
     return (
         <>
-            <Helmet>
-                <title>{t('page.notFound')}</title>
-            </Helmet>
             <div className={css.containerPage404}>
                 <Container>
                     <div className={css.table}>
-                        <h2 className={style.titlePage}>404</h2>
-                        <p>Page Not Found</p>
+                        <Helmet>
+                            <title className={css.titlePage}>
+                                {t('page.notFound')}
+                            </title>
+                        </Helmet>
                     </div>
-                    <div className={css.arrow}></div>
-                    <p className={css.message}>
-                        {`Press "Go Back" to return.`}
-                    </p>
-                    <NavLink to="/" className={css.goBack}>
-                        Go Back
-                    </NavLink>
+                    <motion.h1
+                        className={css.message}
+                        animate={{ color: ['#000000', '#FF0000', '#000000'] }}
+                        transition={{
+                            repeat: Infinity,
+                            duration: 2,
+                            ease: 'easeInOut',
+                        }}
+                    >
+                        Please press the bottom Home
+                    </motion.h1>
+                    <motion.div
+                        className={css.arrow}
+                        animate={{ y: [0, 20, 0] }}
+                        transition={{
+                            repeat: Infinity,
+                            duration: 2,
+                            ease: 'easeInOut',
+                        }}
+                    >
+                        <FaArrowDown size={80} color="red" />
+                    </motion.div>
+                    <Link to="/" className={css.btn}>
+                        Home
+                    </Link>
                 </Container>
             </div>
         </>
